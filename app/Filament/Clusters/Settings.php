ebe72d288a5ba5a270e6e5c74918d72cdd0c67c6
Valpuia <valpuia604@gmail.com>
2024-04-03 10:21:09 +0700

    update settings

--- app/Filament/Clusters/Settings.php
+++ app/Filament/Clusters/Settings.php
@@ -2,12 +2,9 @@
 
 namespace App\Filament\Clusters;
 
-use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Clusters\Cluster;
 
 class Settings extends Cluster
 {
-    use HasPageShield;
-
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
 }
